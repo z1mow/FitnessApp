12205ae9058b13fe12bb28371ae273b22669e5fe
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-01-03 10:14:51 +0300

    fitness activity home screen ui and activity card

---
+++ FitnessApp/Home/ActivityCardView.swift
@@ -0,0 +1,55 @@
+//
+//  ActivityCardView.swift
+//  FitnessApp
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 3.01.2025.
+//
+
+import SwiftUI
+
+struct Activity {
+    let id: Int
+    let title: String
+    let subtitle: String
+    let image: String
+    let tintColor: Color
+    let amount: String
+}
+
+struct ActivityCardView: View {
+    
+    @State var activity: Activity
+    
+    var body: some View {
+        ZStack {
+            Color(uiColor: .systemGray6)
+                .cornerRadius(15)
+            
+            VStack {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(activity.title)
+                        
+                        Text(activity.subtitle)
+                            .font(.caption)
+                    }
+                    
+                    Spacer()
+                    
+                    Image(systemName: activity.image)
+                        .foregroundColor(activity.tintColor)
+                }
+                
+                Text(activity.amount)
+                    .font(.title)
+                    .bold()
+                    .padding()
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ActivityCardView(activity: Activity(id: 0, title: "Today steps", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .green, amount: "9,812"))
+}
--- FitnessApp/Home/HomeView.swift
+++ FitnessApp/Home/HomeView.swift
@@ -12,10 +12,17 @@ struct HomeView: View {
     @State var active: Int = 52
     @State var stand: Int = 8
     
+    var mockActivities = [
+        Activity(id: 1, title: "Walking", subtitle: "Goal 11,000", image: "figure.walk", tintColor: .yellow, amount: "10,453"),
+        Activity(id: 2, title: "Walking", subtitle: "Goal 7,000", image: "figure.walk", tintColor: .blue, amount: "2,193"),
+        Activity(id: 3, title: "Walking", subtitle: "Goal 12,000", image: "figure.walk", tintColor: .pink, amount: "8,750"),
+        Activity(id: 4, title: "Walking", subtitle: "Goal 16,000", image: "figure.walk", tintColor: .green, amount: "11,453")
+    ]
+    
     
     var body: some View {
         ScrollView(showsIndicators: false) {
-            VStack {
+            VStack(alignment: .leading) {
                 Text("Welcome")
                     .font(.largeTitle)
                     .padding()
@@ -63,10 +70,10 @@ struct HomeView: View {
                     ZStack {
                         ProgressCircleView(progress: $calories, goal: 600, color: .red)
                         
-                        ProgressCircleView(progress: $active, goal: 60, color: .green)
+                        ProgressCircleView(progress: $active, goal: 70, color: .green)
                             .padding(.all, 20)
                         
-                        ProgressCircleView(progress: $stand, goal: 12, color: .blue)
+                        ProgressCircleView(progress: $stand, goal: 16, color: .blue)
                             .padding(.all, 40)
                         
                     }
@@ -75,6 +82,32 @@ struct HomeView: View {
                     Spacer()
                 }
                 .padding()
+                
+                HStack {
+                    Text("Fitness Activity")
+                        .font(.title2)
+                    
+                    Spacer()
+                    
+                    Button{
+                        print("Show more")
+                    } label: {
+                        Text("Show more")
+                            .padding(.all, 10)
+                            .foregroundColor(.white)
+                            .background(.blue)
+                            .cornerRadius(20)
+                    }
+                }
+                .padding(.horizontal)
+                
+                LazyVGrid(columns: Array(repeating: GridItem(spacing: 20), count: 2)) {
+                    ForEach(mockActivities, id: \.id) { activity in
+                        ActivityCardView(activity: activity)
+                    }
+                }
+                .padding(.horizontal)
+                
             }
         }
     }
