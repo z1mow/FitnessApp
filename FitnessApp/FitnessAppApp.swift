f9983830e315c3dcef3b542e08c2ca0624737754
Şakir Yılmaz Öğüt <zimowcs@gmail.com>
2025-01-03 09:15:58 +0300

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 3.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
